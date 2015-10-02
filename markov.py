563b7408a456d99c8b68ef342272d097a53ab8e9
Hackbright Student <info@hackbrightacademy.com>
2015-10-01 17:51:30 -0700

    Finished Tweetable section

--- markov.py
+++ markov.py
@@ -4,6 +4,10 @@ from random import choice
 
 class SimpleMarkovGenerator(object):
     chains = {}
+    max_output_size = float("inf")
+
+    def __init__(self,max_output_size):
+        self.max_output_size = max_output_size
 
     def read_files(self, filenames):
         """Given a list of files, make chains from them."""
@@ -40,18 +44,25 @@ class SimpleMarkovGenerator(object):
         """Takes dictionary of markov chains; returns random text."""
         key = choice(self.chains.keys())
         words = [key[0], key[1]]
-        while key in self.chains:
+        num_chars = 0
+        while key in self.chains and len(words) < self.max_output_size:
         # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text)
+        # (which would mean it was tmax_output_sizehe end of our original text)
         #
         # Note that for long texts (like a full book), this might mean
         # it would run for a very long time.
-
             word = choice(self.chains[key])
+            num_chars += len(word)+ 1 #Add the lenght of the word and the space to the list counting characters
             words.append(word)
             key = (key[1], word)
 
         print " ".join(words)
+        print self.max_output_size
+
+
+class TweetableMarkovGenerator(SimpleMarkovGenerator):
+        def __init__(self):
+            return super(TweetableMarkovGenerator,self).__init__(max_output_size=140)
 
 
 if __name__ == "__main__":
@@ -61,9 +72,13 @@ if __name__ == "__main__":
     # we should call the read_files method with the list of filenames
     # we should call the make_text method 5x
     file_list = sys.argv[1:]
-    print file_list
-    generator = SimpleMarkovGenerator()
-    generator.read_files(file_list)
-    for i in range(5):
-        generator.make_text()
-        print i
+    # generator = SimpleMarkovGenerator()
+    # generator.read_files(file_list)
+    # generator.make_text()
+    tweet_generator = TweetableMarkovGenerator()
+    tweet_generator.read_files(file_list)
+    tweet_generator.make_text()
+        
+
+
+
